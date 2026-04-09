4ef70397f7a81cbee051d682a61e90c6b6315d30
Lucas Gomez <gomezlucashernan@hotmail.com>
2026-04-09 16:40:45 -0300

    Dvlop Web SIte MVP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dvlop.agency',
+  image: {
+    domains: ["images.unsplash.com"]
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [icon({
+    include: {
+      ri: ['*']
+    }
+
+  }
+  ), sitemap()]
+});
